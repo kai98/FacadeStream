3200452307824efe32276a9ce1146b3707c0f123
Kai <kaiyuan98@outlook.com>
2021-05-25 18:36:50 -0400

    FacadeStream first commit

---
+++ mainstream.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import torch
+from PIL import Image
+import cv2 as cv
+import os
+from sources.utils import predict
+from sources.utils import init_deeplab
+import torch
+import numpy as np
+
+st.title('DeepLab Facade')
+uploaded_files = st.sidebar.file_uploader('Upload Facade Images', ['png', 'jpg'], accept_multiple_files=True)
+
+filename = []
+wwr_dictionary = {}
+
+def name_without_extension(name):
+    return str(name).split('.')[0]
+
+# Show uploaded_images on the side bar
+for uploaded_file in uploaded_files:
+    img = Image.open(uploaded_file)
+    name = uploaded_file.name
+    plt.imshow(img)
+    plt.show()
+    st.sidebar.image(img, caption=name)
+    filename.append(name_without_extension(name))
+
+
+def clear_folder(folder_path):
+    # Check if the image uploading folder exist
+    # If yes, delete all file under this path
+    if os.path.exists(folder_path):
+        for img_name in os.listdir(folder_path):
+            filepath = os.path.join(folder_path, img_name)
+            print(filepath)
+            os.remove(filepath)
+    # if the folder doesn't exist, create an empty folder.
+    else:
+        os.mkdir(folder_path)
+    return
+
+def save_uploaded_images(input_path):
+    for ufile in uploaded_files:
+        _img = Image.open(ufile)
+        _img = _img.save(input_path+'/'+ufile.name)
+
+    return
+
+def displayPrediction(image_folder):
+    anno_postfix = '_annotation.jpg'
+    pred_postfix = '_prediction.jpg'
+    for i, fn in enumerate(filename):
+        annotation = image_folder + '/' + fn + anno_postfix
+        prediction = image_folder + '/' + fn + pred_postfix
+
+        cols = st.beta_columns(2)
+        cols[0].image(prediction, use_column_width=True,  caption= 'prediction: ' + fn)
+        cols[1].image(annotation, use_column_width=True,  caption= 'Annotation: '+fn,)
+
+
+        # st.image([annotation, prediction], caption=['Annotation: '+fn, 'prediction: ' + fn])
+
+        # get WWR
+        wwr = wwr_dictionary[fn]
+        wwr_percentage = str(round(wwr * 100, 2) ) + "%"
+
+        # Some markdown 
+        st.markdown("> Window-to-Wall Ratio Estimation:  " + "**" + wwr_percentage + "**")
+        st.markdown("------")
+    return
+
+def deeplabv3ModelGenerator(model_path):
+    num_classes = 9
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = init_deeplab(num_classes)
+    state_dict = torch.load(model_path, map_location=device)
+    model = model.to(device)
+    model.load_state_dict(state_dict)
+
+    return model, device
+
+
+def run_prediction():
+    upload_path = './images/upload'
+    prediction_path = './images/prediction'
+    image_path = './images'
+
+    # Clear both input and output folder. 
+    #clear_folder(upload_path)
+    #clear_folder(prediction_path)
+
+    # Save images to input folder
+    save_uploaded_images(upload_path)
+
+    # Generate a model with selected weight
+    model_path = './models/deeplabv3_facade_2k.pth'
+    deeplabv3_model, device = deeplabv3ModelGenerator(model_path)
+
+    # Make prediction
+    for ufile in uploaded_files:
+        img = np.array(Image.open(ufile))
+        ufile_name = ufile.name
+        filename = name_without_extension(ufile_name)
+        # model, image, filename, prediction_path, device
+        wwr_val = predict(deeplabv3_model, img, filename, prediction_path, device)
+        wwr_dictionary[filename] = wwr_val
+
+    # Display prediction and WWR
+    displayPrediction(prediction_path)
+    return
+
+
+if st.button('Analysis'):
+    run_prediction()
---
+++ sources/utils.py
@@ -0,0 +1,164 @@
+import torch
+import numpy as np
+import torchvision
+from torchvision import transforms
+import cv2
+from PIL import Image
+
+transforms_image = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# One image at a time. 
+def predict(model, image, filename, prediction_path, device):
+    # TODO: Multiple image support.
+    # Number of classes in the dataset
+
+    # turn on evaluation 
+    model.eval()
+
+    # make sure image is nparray
+    image = np.array(image)
+
+    prediction_indexed= label_image(model, image, device)
+
+    prediction_color = decode_segmap(prediction_indexed)
+
+    annotation = annotate_image(image, prediction_indexed)
+
+    # make sure in the right format
+    # image = image.astype('uint8')
+
+    # Image channel.. 
+    annotation = cv2.cvtColor(annotation, cv2.COLOR_RGB2BGR)
+    prediction = cv2.cvtColor(prediction_color, cv2.COLOR_RGB2BGR)
+
+    cv2.imwrite(f"./images/prediction/" + filename + "_annotation.jpg", annotation)
+    cv2.imwrite(f"./images/prediction/" + filename + "_prediction.jpg", prediction)
+
+    # Also return the original predicted value. 
+    return get_wwr_by_pixel(prediction_indexed)
+
+
+def get_wwr_by_pixel(prediction_indexed):
+
+    # 0 Void, or various
+    # 1 Wall
+    # 2 Car
+    # 3 Door
+    # 4 Pavement
+    # 5 Road
+    # 6 Sky
+    # 7 Vegetation
+    # 8 Windows
+    window_count = np.sum(prediction_indexed == 8)
+    wall_count = np.sum(prediction_indexed == 1)
+    door_count = np.sum(prediction_indexed == 3)
+
+    return window_count / (window_count + wall_count + door_count)
+
+
+def annotate_image(image, pred_indexed):
+
+    annotate_colors = {
+        0 : (0, 0, 0),              # Various
+        1 : (128, 0, 0),            # Wall
+        2 : (128, 0, 128),          # Car
+        3 : (128, 128, 0),          # Door
+        4 : (128, 128, 128),        # Pavement
+        5 : (128, 64, 0),           # Road
+        6 : (0, 128, 128),          # Sky
+        7 : (0, 128, 0),            # Vegetation
+        8 : (0, 0, 128)             # Windows
+    }
+    image = np.array(image)
+    
+    # 
+    dim_factor = 0.5
+    image = image * dim_factor
+
+    r = image[:, :, 0]
+    g = image[:, :, 1]
+    b = image[:, :, 2]
+
+    anno_factor = 0.5
+
+    for l in annotate_colors:
+        idx = pred_indexed == l
+        r[idx] += annotate_colors[l][0] * anno_factor
+        g[idx] += annotate_colors[l][1] * anno_factor
+        b[idx] += annotate_colors[l][2] * anno_factor
+
+    rgb = np.stack([r, g, b], axis=2)
+    rgb = rgb.clip(0, 255)
+    image = rgb.astype('uint8')
+    return image
+
+
+# Define the helper function
+def decode_segmap(pred_indexed, nc=9):
+
+
+    # 0 Void, or various
+    # 1 Wall
+    # 2 Car
+    # 3 Door
+    # 4 Pavement
+    # 5 Road
+    # 6 Sky
+    # 7 Vegetation
+    # 8 Windows
+
+    label_colors = {
+        0 : (0, 0, 0),              # Various
+        1 : (128, 0, 0),            # Wall
+        2 : (128, 0, 128),          # Car
+        3 : (128, 128, 0),          # Door
+        4 : (128, 128, 128),        # Pavement
+        5 : (128, 64, 0),           # Road
+        6 : (0, 128, 128),          # Sky
+        7 : (0, 128, 0),            # Vegetation
+        8 : (0, 0, 128)             # Windows
+    }
+
+
+    r = np.zeros_like(pred_indexed).astype(np.uint8)
+    g = np.zeros_like(pred_indexed).astype(np.uint8)
+    b = np.zeros_like(pred_indexed).astype(np.uint8)
+    
+
+    for l in range(0, nc):
+        idx = pred_indexed == l
+        r[idx] = label_colors[l][0]
+        g[idx] = label_colors[l][1]
+        b[idx] = label_colors[l][2]
+    
+    rgb = np.stack([r, g, b], axis=2)
+    
+    return rgb
+
+
+def label_image(model, image, device):
+    image = transforms_image(image)
+    image = image.unsqueeze(0)
+    image_np = np.asarray(image)
+
+    image = image.to(device)
+    outputs = model(image)["out"]
+
+    _, preds = torch.max(outputs, 1)
+
+    preds = preds.to("cpu")
+
+    preds_np = preds.squeeze(0).cpu().numpy().astype(np.uint8)
+
+    return preds_np
+
+def init_deeplab(num_classes):
+
+    model_deeplabv3 = torchvision.models.segmentation.deeplabv3_resnet101()
+    model_deeplabv3.aux_classifier = None
+    model_deeplabv3.classifier = torchvision.models.segmentation.deeplabv3.DeepLabHead(2048, num_classes)
+
+    return model_deeplabv3
